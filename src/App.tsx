bbca31c6908b3adfa945eb216412fd375e44935b
GGeonu <chrismin7030@naver.com>
2019-11-17 21:22:27 +0900

    feature: first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import './App.css';
+
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className="App">
+        <div> hello </div>
+      </div>
+    );
+  }
+}
+
+export default App;
